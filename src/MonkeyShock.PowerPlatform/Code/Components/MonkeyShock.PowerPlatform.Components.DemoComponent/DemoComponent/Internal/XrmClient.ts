97be2cb5db1e9977a9f57b26b231579e9e5509fb
gashupl <piotrek.gaszewski@hotmail.com>
2021-09-02 15:53:48 +0200

    Feature/web api calls (#3)
    * Function for account creation added
    
    * Updated manifest control version
    
    Co-authored-by: Piotr Gaszewski <piog@netcompany.com>

--- src/MonkeyShock.PowerPlatform/Code/Components/MonkeyShock.PowerPlatform.Components.DemoComponent/DemoComponent/Internal/XrmClient.ts
+++ src/MonkeyShock.PowerPlatform/Code/Components/MonkeyShock.PowerPlatform.Components.DemoComponent/DemoComponent/Internal/XrmClient.ts
@@ -1,17 +1,40 @@
 
 declare var Xrm: any;
 
-export class XrmClient{
-    
-    constructor() {  }
+export class XrmClient {
 
-    getUrl() : void{
-        if((<any>Xrm) !== 'undefined'){
+    constructor() { }
+
+    getUrl(): void {
+        if ((<any>Xrm) !== 'undefined') {
             var url: string = (<any>Xrm).Utility.getGlobalContext().getClientUrl();
-            alert(url); 
+            alert(url);
         }
-        else{
-            alert("Xrm is not defined!"); 
-        }     
+        else {
+            alert("Xrm is not defined!");
+        }
+    }
+
+    createAccount(): void {
+
+        let createdOn = Date.now();
+
+        // define the data to create new account
+        let data =
+        {
+            "name": "Sample Account created from PCF control " +  createdOn,
+            "description": "This is the description of the sample account"
+        }
+
+        // create account record
+        Xrm.WebApi.createRecord("account", data).then(
+            function success(result: any) {
+                alert("Account created with ID: " + result.id);
+                // perform operations on record creation
+            },
+            function (error : any) {
+                alert(error.message);
+            }
+        );
     }
 }

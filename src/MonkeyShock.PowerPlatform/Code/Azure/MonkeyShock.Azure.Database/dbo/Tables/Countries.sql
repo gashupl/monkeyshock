f2e4988a98b6af6d6249d8c76fd572de00818268
gashupl <piotrek.gaszewski@hotmail.com>
2021-11-16 16:06:30 +0100

    Iteration02/feature/sql db setup (#11)
    * Azure Resource Group project added to solution
    
    * ARM Templates updates
    
    * Database name modified
    
    * Sql Server project added to the solution
    
    * Countries table added to db project
    
    Co-authored-by: Piotr Gaszewski <piog@netcompany.com>

---
+++ src/MonkeyShock.PowerPlatform/Code/Azure/MonkeyShock.Azure.Database/dbo/Tables/Countries.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Countries] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (256) NOT NULL,
+    CONSTRAINT [PK_Countries] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
